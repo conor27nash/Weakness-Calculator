deb5319c06937ed2af0e96370230a187de78deda
Conor Nash <conorn@forrit.com>
2026-04-10 10:56:54 +0100

    Update node action versions

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Weakness-Calculator/',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
